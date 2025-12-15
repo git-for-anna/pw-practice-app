1f8226b7109d674ae50d914d14c64e4cecc8ab7f
Anna <135lee@usa.com>
2025-12-14 22:51:01 -0500

    New lesson

---
+++ tests/childelementsLesson.spec.ts
@@ -0,0 +1,13 @@
+import { test} from '@playwright/test'
+
+
+test.beforeEach(async({page})=>{
+    await page.goto('http://localhost:4200')
+    await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
+})
+
+test('Child Elements Lesson', async({page})=>{
+
+ 
+})

9833985b1c2f9779f60a08d1478be1f7c7d1babe
Anna <135lee@usa.com>
2025-12-21 20:29:44 -0500

    done with this lesson

--- tests/childelementsLesson.spec.ts
+++ tests/childelementsLesson.spec.ts
@@ -7,7 +7,13 @@ test.beforeEach(async({page})=>{
     await page.getByText('Form Layouts').click()
 })
 
-test('Child Elements Lesson', async({page})=>{
+test('locating child elements', async({page})=>{
+    // await page.locator('nb-card nb-radio :test-is("Option 1")').click()
+    await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click()
+
+    await page.locator('nb-card').getByRole('button', {name: "Sign in"}).first().click()
+
+    await page.locator('nb-card').nth(3).getByRole('button').click() // index.. remember it always starts with the 0 . Try to avoid this method
 
  
 })

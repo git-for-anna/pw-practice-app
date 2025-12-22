1fd514e8982702c18e71abbf94b5899f046b7f10
Anna <135lee@usa.com>
2025-12-21 22:53:00 -0500

    gettingTheTextfromelements Lesson is done

---
+++ tests/gettingTextFromWebElements.spec.ts
@@ -0,0 +1,31 @@
+import { expect, test} from '@playwright/test'
+
+
+test.beforeEach(async({page})=>{
+    await page.goto('http://localhost:4200')
+    await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
+})
+
+test('extracting values', async({page})=>{
+    //single text value
+     const basicForm = page.locator('nb-card').filter({hasText: "Basic form"})
+    const buttontext = await basicForm.locator('button').textContent()
+    expect(buttontext).toEqual('Submit')
+
+    //all text values
+   const allRadioButtonsLabels = await page.locator('nb-radio').allTextContents()
+   expect(allRadioButtonsLabels).toContain("Option 1")
+
+   //input value
+   const emailField = basicForm.getByRole('textbox', {name:"Email"})
+   await emailField.fill('test@test.com')
+  const emailValue =  await emailField.inputValue()
+  expect(emailValue).toEqual('test@test.com')
+
+  //get the value of the attribute
+
+  const placeholderValue = await emailField.getAttribute('placeholder')
+  expect(placeholderValue).toEqual('Email')
+
+})
